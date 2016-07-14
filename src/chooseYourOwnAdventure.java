509f15e30d87e39b709943bc6322ce341d4f2194
Ryon <league@Mac-mini-3.local>
2016-07-13 17:32:21 -0700

    today was a little hard yet great

---
+++ src/chooseYourOwnAdventure.java
@@ -0,0 +1,10 @@
+import javax.swing.JOptionPane;
+public class chooseYourOwnAdventure {
+	public static void main(String[] args) {
+		String rather=JOptionPane.showInputDialog(null, "Do you want to go to the vegas or hawaii?");
+String vegas=JOptionPane.showInputDialog(null, "Would you rather be in a hotel or outside");
+
+		
+	}
+
+}
