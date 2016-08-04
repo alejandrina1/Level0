9b47d261189b3395027232f259f6a0efd20537d5
alejandrina1 <league@FunkyMini.local>
2016-08-03 17:31:58 -0700

    today was okay

---
+++ src/exercises.java
@@ -0,0 +1,10 @@
+
+public class exercises {
+	public static void main(String[] args) {
+		
+for(int i=50;i>=20;i--){
+	System.out.println(i);
+}
+}
+}
+
--- src/soundtrackforlife.java
+++ src/soundtrackforlife.java
@@ -7,16 +7,25 @@ public class soundtrackforlife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "What mood are you in ?", "Mood", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Cheerful", "Upset", "Angry" }, null);
-		
+		int userMood = JOptionPane.showOptionDialog(null, "What mood are you in ?", "Mood", 0, JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Cheerful", "Upset", "Angry" },
+				null);
+
 		// 2. Their answer is stored in the userMood variable. Print it out.
-System.out.println(userMood);
+		System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-if (userMood==0) {
-	System.out.println("playVideo happy");
-playVideo(" https://www.youtube.com/watch?v=3IIspaicSnY");
-}
+		if (userMood == 0) {
+			System.out.println("playVideo cheerful");
+			playVideo("https://www.youtube.com/watch?v=3IIspaicSnY");
+		}
+		if (userMood == 1){
+			System.out.println("playVideo upset");
+			playVideo("https://www.youtube.com/watch?v=fk4BbF7B29w");
+		}
+		
+	if (userMood == 2) {
+		System.out.println("playVideo angry");
+		playVideo("https://www.youtube.com/watch?v=sIjkVn_ro0g&list=PLqtXP8v3VfDAXpMFOhZ-lZqK1neQ4IC_U&index=46");
+	}
 		// 4. Play different songs for other moods.
 
 		// If you are seeing ads at the beginning of your videos, install Adblock.
@@ -33,12 +42,7 @@ playVideo(" https://www.youtube.com/watch?v=3IIspaicSnY");
 	}
 }
 
-
-
-
 /**
-* If you can’t think of any, here are some you can use...
-*	bit.ly/video-for-happy
-*	bit.ly/video-for-sad
-*	bit.ly/video-for-angry
-**/
+ * If you can’t think of any, here are some you can use... bit.ly/video-for-happy bit.ly/video-for-sad
+ * bit.ly/video-for-angry
+ **/
